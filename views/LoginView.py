32d5ad20487e852092cfc9a4bb93fedc332f40f2
Oscar Buitrago <o.buitragov@uniandes.edu.co>
2023-10-14 02:18:27 -0500

    Crea Estructura de Proyecto Closes #1

---
+++ views/LoginView.py
@@ -0,0 +1,6 @@
+from flask_restful import Resource
+
+
+class LoginView(Resource):
+    def post(self):
+        return "No implementado", 500
---
+++ views/SignUpView.py
@@ -0,0 +1,6 @@
+from flask_restful import Resource
+
+
+class SignUpView(Resource):
+    def post(self):
+        return "No implementado", 500
